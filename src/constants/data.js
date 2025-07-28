3cfb3cd60020625a798896045a8cc2ee24b8149d
Jeremy <jeremythegreat95@gmail.com>
2025-07-28 13:25:30 +0800

    built archive page

--- src/constants/data.js
+++ src/constants/data.js
@@ -21,31 +21,12 @@ const workExperiences = [
 
 const listOfProjects = [
     {
-        id: 'p3',
-        project_title: 'Apple Demo Website',
-        project_description: 'A visually rich and interactive experience with fancy scrolling animations developed using GSAP and Three.js.',
-        project_techstack: ['React.js', 'GSAP', 'Three.js', 'Netlify'],
-        project_link: "https://react-gsap-iphone-website-demo.netlify.app/",
-        project_source_code: "https://github.com/jeremyerikleong/react-apple-website",
-        project_screenshot: '/assets/projects/project-3.webp'
-    },
-    {
         id: 'p2',
         project_title: 'Blogger',
         project_description: 'A full-stack blog application developed using PERN stack.',
         project_techstack: ['React.js', 'Tailwind CSS', 'Node.js', 'Express', 'PostgreSQL', 'AWS', 'Railway'],
         project_link: "https://react-blogger.up.railway.app/login",
-        project_source_code: "https://github.com/jeremyerikleong/blogger-frontend",
         project_screenshot: '/assets/projects/project-2.webp'
-    },
-    {
-        id: 'p1',
-        project_title: 'Music Player',
-        project_description: 'A basic music player developed using vanilla JavaScript.',
-        project_techstack: ['HTML', 'SCSS', 'JavaScript', 'Github Pages'],
-        project_link: "https://jeremyerikleong.github.io/music-player/",
-        project_source_code: "https://github.com/jeremyerikleong/music-player",
-        project_screenshot: '/assets/projects/project-1.webp'
     },
     {
         id: 'p4',
@@ -53,7 +34,6 @@ const listOfProjects = [
         project_description: 'A cooking web app designed for home chefs and foodies, with easy, tasty homemade recipes.',
         project_techstack: ['React.js', 'Tailwind CSS', 'Vercel'],
         project_link: "https://recipe-app-two-ebon.vercel.app/",
-        project_source_code: "https://github.com/jeremyerikleong/recipe-app",
         project_screenshot: '/assets/projects/project-4.webp'
     },
     {
@@ -62,7 +42,6 @@ const listOfProjects = [
         project_description: 'A client website developed using Next.js framework.',
         project_techstack: ['Next.js', 'Tailwind CSS', 'Framer Motion', 'Vercel'],
         project_link: "https://www.theboringdevs.com/",
-        project_source_code: "https://github.com/jeremyerikleong/next-js-company-website",
         project_screenshot: '/assets/projects/project-5.webp'
     },
     {
@@ -71,9 +50,83 @@ const listOfProjects = [
         project_description: 'A light-aquamarine, cyberpunk-inspired theme for Visual Studio Code.',
         project_techstack: [],
         project_link: "https://cyberpunk-aquamarine-theme.netlify.app/",
-        project_source_code: "https://github.com/jeremyerikleong/cyberpunk-light-aquamarine-theme",
         project_screenshot: '/assets/projects/project-6.webp'
     }
 ]
 
-export { workExperiences, listOfProjects };
+const archivedProjectTableHeader = ['year', 'project', 'built using', 'link']
+
+const archivedProjectTableData = [
+    {
+        project_id: 'td1',
+        project_year: 2025,
+        project_title: 'My Next.js Resume',
+        project_techstack: ['Next.js', 'Tailwind CSS', 'Netlify'],
+        project_link: 'cv.jeremyerikleong.com',
+    },
+    {
+        project_id: 'td2',
+        project_year: 2025,
+        project_title: 'Apple Demo Website',
+        project_techstack: ['React.js', 'Tailwind CSS', 'GSAP', 'Three.js', 'Netlify'],
+        project_link: 'react-gsap-iphone-website-demo.netlify.app',
+    },
+    {
+        project_id: 'td3',
+        project_year: 2025,
+        project_title: 'Anime List',
+        project_techstack: ['React.js', 'Tailwind CSS', 'Appwrite', 'Vercel'],
+        project_link: 'react-anime-list-lime.vercel.app',
+    },
+    {
+        project_id: 'td4',
+        project_year: 2025,
+        project_title: 'Talent Weave',
+        project_techstack: ['HTML', 'CSS', 'Bootstrap 5', 'JavaScript', 'Swiper', 'AOS', 'Netlify'],
+        project_link: 'talent-weave-demo.netlify.app',
+    },
+    {
+        project_id: 'td5',
+        project_year: 2024,
+        project_title: 'Pokédex Demo',
+        project_techstack: ['HTML', 'SCSS', 'JavaScript', 'Netlify'],
+        project_link: 'pokedex-demo-v2.netlify.app',
+    },
+    {
+        project_id: 'td6',
+        project_year: 2023,
+        project_title: 'Luxberry Training',
+        project_techstack: ['HTML', 'CSS', 'Bootstrap 5', 'JavaScript'],
+        project_link: 'www.luxberrytraining.com',
+    },
+    {
+        project_id: 'td7',
+        project_year: 2022,
+        project_title: 'Music Player',
+        project_techstack: ['HTML', 'SCSS', 'JavaScript', 'Github Page'],
+        project_link: 'jeremyerikleong.github.io/music-player',
+    },
+    {
+        project_id: 'td8',
+        project_year: 2022,
+        project_title: 'Wordle JS',
+        project_techstack: ['HTML', 'SCSS', 'JavaScript', 'Netlify'],
+        project_link: 'simple-wordle-js.netlify.app',
+    },
+    {
+        project_id: 'td9',
+        project_year: 2022,
+        project_title: 'Simon',
+        project_techstack: ['HTML', 'SCSS', 'jQuery', 'Netlify'],
+        project_link: 'simon-game-classic.netlify.app',
+    },
+    {
+        project_id: 'td10',
+        project_year: 2022,
+        project_title: 'My Portfolio (v1)',
+        project_techstack: ['HTML', 'SCSS', 'JavaScript', 'Parcel', 'Netlify'],
+        project_link: 'v1.jeremyerikleong.com',
+    }
+]
+
+export { workExperiences, listOfProjects, archivedProjectTableHeader, archivedProjectTableData };
---
+++ src/pages/Archive.jsx
@@ -0,0 +1,83 @@
+import { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { archivedProjectTableHeader, archivedProjectTableData } from '../constants/data';
+import { isTablet, isBrowser } from 'react-device-detect';
+import { Helmet } from 'react-helmet-async';
+
+export default function Archive() {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [location]);
+
+    return (
+        <>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Archive | Jeremy Erik Leong</title>
+                <link rel="canonical" href="https://jeremyerikleong.com/archive" />
+                <meta name="description" content="Hello! My name is Jeremy Erik Leong. I’m a developer & designer that based in Kuala Lumpur. I enjoy building digital experiences for the web." />
+            </Helmet>
+
+            <main className="archive-wrapper">
+                <div className="navigation-container">
+                    <a className="btn-navigation" onClick={() => navigate('/')}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="arrow-left" aria-hidden="true"><path fillRule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clipRule="evenodd"></path></svg>
+                        Jeremy Erik Leong
+                    </a>
+                    <h1 className="hero-title">All Projects</h1>
+                </div>
+
+                <div className="table-responsive-container">
+                    <table className="responsive-table">
+                        <thead>
+                            <tr>
+                                {archivedProjectTableHeader.map((header, index) => (
+                                    <th key={index}>{header}</th>
+                                ))}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {archivedProjectTableData.map(data => (
+                                <tr key={data.project_id}>
+                                    <td className="project-year">{data.project_year}</td>
+
+                                    {isTablet || isBrowser ? <td className="project-name">{data.project_title}</td> :
+                                        <td className="project-name">
+                                            <span>
+                                                <a href={`https://${data.project_link}`} target="_blank">
+                                                    {data.project_title}
+                                                </a>
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="arrow-up-right" aria-hidden="true"><path fillRule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clipRule="evenodd"></path></svg>
+                                            </span>
+                                        </td>}
+
+                                    <td>
+                                        <ul className="archive-list-container">
+                                            {data.project_techstack.map((techstack, index) => (
+                                                <li key={index} className="archive-list-techstack">
+                                                    {techstack}
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    </td>
+
+                                    <td>
+                                        <span>
+                                            <a href={`https://${data.project_link}`} target="_blank">
+                                                {data.project_link}
+                                            </a>
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="arrow-up-right" aria-hidden="true"><path fillRule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clipRule="evenodd"></path></svg>
+                                        </span>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </main>
+        </>
+    )
+}
